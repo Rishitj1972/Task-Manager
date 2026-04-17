f2b25edaa0f49832cccf0eadfef3dd1873669245
Rishi <rishitj1972@gmail.com>
2026-04-17 20:52:09 +0530

    Run frontend Create Login.jsx Add email + password inputs Show it in browser

---
+++ TaskManager-Frontend/src/components/Login.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+function Login() {
+    
+    const [email,setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <input
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input
+                type="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default Login;
